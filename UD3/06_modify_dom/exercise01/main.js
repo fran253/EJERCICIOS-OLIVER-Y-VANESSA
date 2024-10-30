4852eb50f849d3216cb6f6a5f486edbcfe4a88ce
fran253 <123721486+fran253@users.noreply.github.com>
2024-10-30 21:50:20 +0100

    Ejercicios 5 y 6 U3

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,61 @@
 window.onload = function(e) {
     console.log('documento cargado')
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+//Cambiar el titulo del segundo h2
+    document.getElementsByTagName('h1')[1].innerText = 'Changed from JS!!'
+   
+    
+
+// Seleccionar el elemento con id == username
+    const usernameElement = document.getElementById('username');
+    console.log(usernameElement); 
+    
+
+
+// Cambiar el color de todos los .first que estén dentro de un artículo
+    const allFirst = document.querySelectorAll('article .first')
+    for (let item of allFirst) {
+        item.computedStyleMap['background-color'] = 'green'
+    }
+
+
+//Seleccionar todos los elementos li con class == item
+    const liElement = document.querySelectorAll('li.item')
+    console.log(liElement)
+
+
+//Seleccionar todos los buttons dentro de class == buttons
+    const buttons = document.querySelectorAll('.buttons button')
+    document.querySelectorAll('p')[0].style
+    ['background-color'] = 'ffccff'
+
+
+//Cambiar el color de frente de los elementos buttons dentro de class == buttons
+    for (let item of allButtons) {
+        item.style.color = 'red'
+    }
+
+/////////////////////////////////////////////////
+
+let firstArticle = document.querySelector('article')
+
+    let node = document.createElement('p')
+    node.innerText = 'Hola mundo añadiendo nodos en el árbol DOM'    
+    node.style['background-color'] = 'grey'
+    node.title = 'Párrafo de ejemplo'
+
+    let aNode = document.createElement('a')
+    aNode.innerText = 'Enlace de ejemplo'
+    aNode.href= 'https://www.google.es'
+
+    node.appendChild(aNode)
+
+    let fullNode = document.createElement('div')
+fullNode.innerHTML = `Esto es un div de ejemplo
+    que lleva un link <a href="http://www.marca.es">Marca</a>`
+
+    node.appendChild(fullNode)
+
+    firstArticle.appendChild(node)
 }
+    
--- UD3/06_modify_dom/table/main.js
+++ UD3/06_modify_dom/table/main.js
@@ -1,19 +1,38 @@
+let idCounter = 1; // Variable global para ID autoincremental
+
 function onClick() {
-    let tbody = document.getElementsByTagName('tbody')[0]
+    let tbody = document.getElementsByTagName('tbody')[0];
 
-    let tr = document.createElement('tr')
-    let td = document.createElement('td')
-    td.innerText = '0'
-    tr.appendChild(td)
+    let tr = document.createElement('tr');
 
-    td = document.createElement('td')
-    td.innerText = 'Example'
-    tr.appendChild(td)
+    // columna ID
+    let td = document.createElement('td');
+    td.innerText = idCounter;
+    tr.appendChild(td);
 
-    td = document.createElement('td')
-    td.innerText = 'Surname'
-    tr.appendChild(td)
+    // columna nombre
+    td = document.createElement('td');
+    td.innerText = 'Example';
+    tr.appendChild(td);
 
-    tbody.appendChild(tr)
-    console.log('add')
+    // columna apellido
+    td = document.createElement('td');
+    td.innerText = 'Surname';
+    tr.appendChild(td);
+
+    // columna acciones
+    td = document.createElement('td');
+    let editLink = document.createElement('a');
+    editLink.href = `http://localhost/edit/${idCounter}`;
+    editLink.innerText = 'Editar';
+    td.appendChild(editLink);
+    tr.appendChild(td);
+
+    tbody.appendChild(tr);
+    console.log('Fila añadida con ID:', idCounter);
+
+    idCounter++; // ID autoincremental
 }
+
+//ESTE COMANDO SOBRA PERO MOLA QUE TE AÑADA FILAS SIN PARAR
+//window.setInterval(onClick, 0.01);
