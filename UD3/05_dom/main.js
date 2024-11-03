613682b54234a85d51d7adc91391d8f5babf2988
fran253 <123721486+fran253@users.noreply.github.com>
2024-11-03 19:45:31 +0100

    Ejercicio U3 5

--- UD3/05_dom/main.js
+++ UD3/05_dom/main.js
@@ -1,8 +1,21 @@
 window.onload = () => {
+    // Listar todos los divs
     const allDivs = document.querySelectorAll('div');
+    console.log("Todos los divs:");
+    allDivs.forEach(div => console.log(div));
 
-    for (let div of allDivs) {
-        console.log(div);
-    }
-    const allButtons = document.querySelectorAll('.buttons')
-}
+    // Listar todos los elementos con clase 'buttons'
+    const allButtonsElements = document.querySelectorAll('.buttons');
+    console.log("Todos los elementos con clase 'buttons':");
+    allButtonsElements.forEach(button => console.log(button));
+
+    // Listar 1 div con clase 'buttons'
+    const oneButtonDiv = document.querySelector('div.buttons');
+    console.log("Un div con clase 'buttons':");
+    console.log(oneButtonDiv);
+
+    // Listar TODOS los divs con clase 'buttons'
+    const allButtonDivs = document.querySelectorAll('div.buttons');
+    console.log("Todos los divs con clase 'buttons':");
+    allButtonDivs.forEach(div => console.log(div));
+};
